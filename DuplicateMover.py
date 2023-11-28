8d093a67cd5605ed0387cfa68ef11e4c7b226b36
Mosses Ross <mosasross@gmail.com>
2023-11-28 20:35:42 +0530

    Create DuplicateMover.py

---
+++ DuplicateMover.py
@@ -0,0 +1,24 @@
+from tkinter import Tk
+from tkinter.filedialog import askdirectory
+import os, shutil, hashlib
+
+def DuplicateMover():
+  Tk().withdraw()
+  path = askdirectory(title = "Please select the folder")
+  walker = os.walk(path)
+  uniquefiles=dict()
+  print("Don't close the program")
+  for folder, sub_folder, files in walker:
+    for file in files:
+      filepath = os.path.join(folder, file)
+      filehash = hashlib.md5(open(filepath,"rb").read()).hexdigest()
+
+      if filehash in uniquefiles:
+        shutil.move(filepath,'D:\\Duplicates\\')
+        print(f"{filepath} is moved to duplicates.")
+      else:
+        uniquefiles[filehash]=path
+      
+  print("Exit code = 0\nProgram finished without errors.")
+
+DuplicateMover()
